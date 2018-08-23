0fe58c89e0212f1843ead50faa99a19f146e42db
Yan Huang (AZURE DEV) <yanh@microsoft.com>
2018-08-24 00:10:10 +0800

    Added concurrency samples.

---
+++ ConcurrencySamples/Program.cs
@@ -0,0 +1,143 @@
+﻿namespace ConcurrencySamples
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal class Program
+    {
+        private static int runningTasks;
+
+        private static int totalExecutedTasks;
+
+        private static volatile bool printInfoExitFlag;
+
+        private static void Main(string[] args)
+        {
+            ThreadPool.GetMaxThreads(out int maxWorkerThreads, out int maxCompletionPortThreads);
+            ThreadPool.GetMinThreads(out int minWorkerThreads, out int minCompletionPortThreads);
+            Console.WriteLine($"Worker Threads: {minWorkerThreads} - {maxWorkerThreads}");
+            Console.WriteLine($"Completion Port Threads: {minCompletionPortThreads} - {maxCompletionPortThreads}");
+            Console.WriteLine("Press any key to continue...");
+            var key = Console.ReadKey();
+
+            var printInfoThread = new Thread(PrintInformation);
+            printInfoThread.Start();
+
+            switch (key.Key)
+            {
+                case ConsoleKey.D1:
+                    ThreadPoolExecuteTask(DoWorkWithBlock);
+                    break;
+
+                case ConsoleKey.D2:
+                    ThreadPoolExecuteTask(DoWorkNoBlock);
+                    break;
+
+                case ConsoleKey.D3:
+                    ThreadPoolExecuteTask(AsyncDoWorkInAPM);
+                    break;
+
+                case ConsoleKey.D4:
+                    TaskRunExecuteTask();
+                    break;
+            }
+
+            printInfoExitFlag = true;
+            printInfoThread.Join();
+        }
+
+        /*
+         * https://docs.microsoft.com/en-us/dotnet/standard/asynchronous-programming-patterns/ 
+        */
+
+        private static void ThreadPoolExecuteTask(WaitCallback doWorkCallback)
+        {
+            const int TotalTasks = 1000;
+            for (int i = 0; i < TotalTasks; i++)
+            {
+                ThreadPool.QueueUserWorkItem(doWorkCallback, null);
+            }
+
+            while (totalExecutedTasks < TotalTasks)
+            {
+                Thread.Sleep(500);
+            }
+        }
+
+        private static void TaskRunExecuteTask()
+        {
+            const int TotalTasks = 1000;
+            Task[] tasks = new Task[TotalTasks];
+            for (int i = 0; i < TotalTasks; i++)
+            {
+                tasks[i] = Task.Run(AsyncDoWorkInTAP);
+            }
+
+            Task.WaitAll(tasks);
+        }
+
+        private static void DoWorkNoBlock(object state)
+        {
+            Interlocked.Increment(ref runningTasks);
+            BusyDoWork(3);
+            Interlocked.Decrement(ref runningTasks);
+            Interlocked.Increment(ref totalExecutedTasks);
+        }
+
+        private static void DoWorkWithBlock(object state)
+        {
+            Interlocked.Increment(ref runningTasks);
+            Thread.Sleep(1000);
+            BusyDoWork(2);
+            Interlocked.Decrement(ref runningTasks);
+            Interlocked.Increment(ref totalExecutedTasks);
+        }
+
+        private static void AsyncDoWorkInAPM(object state)
+        {
+            // APM: https://docs.microsoft.com/en-us/dotnet/standard/asynchronous-programming-patterns/asynchronous-programming-model-apm
+            Interlocked.Increment(ref runningTasks);
+            new Timer((_) =>
+            {
+                BusyDoWork(2);
+                Interlocked.Decrement(ref runningTasks);
+                Interlocked.Increment(ref totalExecutedTasks);
+            }, null, 1000, -1);
+        }
+
+        private static async Task AsyncDoWorkInTAP()
+        {
+            // TAP: https://docs.microsoft.com/en-us/dotnet/standard/asynchronous-programming-patterns/task-based-asynchronous-pattern-tap
+            Interlocked.Increment(ref runningTasks);
+            await Task.Delay(1000);
+            BusyDoWork(2);
+            Interlocked.Decrement(ref runningTasks);
+            Interlocked.Increment(ref totalExecutedTasks);
+        }
+
+        private static void BusyDoWork(double seconds)
+        {
+            long sum = 0, k = 0;
+            DateTime runUntil = DateTime.UtcNow.AddSeconds(seconds);
+            while (runUntil > DateTime.UtcNow)
+            {
+                k++;
+                sum += k;
+            }
+        }
+
+        private static void PrintInformation()
+        {
+            while (!printInfoExitFlag)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"Running Tasks: {runningTasks}, Completed Tasks: {totalExecutedTasks}, Current Threads: {Process.GetCurrentProcess().Threads.Count}");
+            }
+        }
+    }
+}
---
+++ ConcurrencySamples/WriteFileSample.cs
@@ -0,0 +1,125 @@
+﻿namespace ConcurrencySamples
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal class SyncWriteFileSample
+    {
+        private static void ExecuteSync()
+        {
+            using (FileStream fs = new FileStream("somefile", FileMode.Create, FileAccess.ReadWrite))
+            {
+                byte[] buffer = new byte[256];
+                Random r = new Random();
+                r.NextBytes(buffer);
+                fs.Write(buffer, 0, 256);
+                byte[] hash = MD5.Create().ComputeHash(buffer);
+                fs.Write(hash, 0, hash.Length);
+            }
+        }
+    }
+
+    internal class AsyncWriteFileSampleInAPM : IDisposable
+    {
+        private byte[] buffer;
+        private FileStream fs;
+        private volatile bool completed;
+
+        public static void Execute()
+        {
+            using (var instance = new AsyncWriteFileSampleInAPM())
+            {
+                instance.BeginExecute();
+                while (!instance.completed)
+                {
+                    Thread.Sleep(500);
+                }
+            }
+        }
+
+        private void BeginExecute()
+        {
+            this.fs = new FileStream("somefile", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite, 4096, FileOptions.Asynchronous);
+            this.buffer = new byte[256];
+            Random r = new Random();
+            r.NextBytes(this.buffer);
+            this.fs.BeginWrite(this.buffer, 0, 256, Callback1, null);
+        }
+
+        private void Callback1(IAsyncResult result)
+        {
+            this.fs.EndWrite(result);
+            byte[] hash = MD5.Create().ComputeHash(this.buffer);
+            this.fs.BeginWrite(hash, 0, hash.Length, Callback2, null);
+        }
+
+        private void Callback2(IAsyncResult result)
+        {
+            this.fs.EndWrite(result);
+            this.completed = true;
+        }
+
+        public void Dispose()
+        {
+            this.fs.Dispose();
+        }
+    }
+
+    internal class AsyncWriteFileSampleInAPM2
+    {
+        public static void Execute()
+        {
+            var iterator = ExecuteIterator().GetEnumerator();
+            Callback(iterator, false);
+        }
+
+        private static IEnumerable<IAsyncResult> ExecuteIterator()
+        {
+            using (FileStream fs = new FileStream("somefile", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite, 4096, FileOptions.Asynchronous))
+            {
+                byte[] buffer = new byte[256];
+                Random r = new Random();
+                r.NextBytes(buffer);
+                var asyncResult = fs.BeginWrite(buffer, 0, 256, null, null);
+                yield return asyncResult;
+                fs.EndWrite(asyncResult);
+                byte[] hash = MD5.Create().ComputeHash(buffer);
+                asyncResult = fs.BeginWrite(hash, 0, hash.Length, null, null);
+                yield return asyncResult;
+                fs.EndWrite(asyncResult);
+            }
+        }
+
+        private static void Callback(object state, bool timedOut)
+        {
+            var iterator = (IEnumerator<IAsyncResult>)state;
+            if (iterator.MoveNext())
+            {
+                ThreadPool.RegisterWaitForSingleObject(iterator.Current.AsyncWaitHandle, Callback, iterator, -1, true);
+            }
+        }
+    }
+
+
+    internal class AsyncWriteFileSampleInTAP
+    {
+        private static async Task ExecuteAsync()
+        {
+            using (FileStream fs = new FileStream("somefile", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite, 4096, FileOptions.Asynchronous))
+            {
+                byte[] buffer = new byte[256];
+                Random r = new Random();
+                r.NextBytes(buffer);
+                await fs.WriteAsync(buffer, 0, 256);
+                byte[] hash = MD5.Create().ComputeHash(buffer);
+                await fs.WriteAsync(hash, 0, hash.Length);
+            }
+        }
+    }
+}
